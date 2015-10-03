94a9d02bcef641f8359dc554b3ececceeb12eced
jorgedms <man13600@uvg.edu.gt>
2015-10-03 14:02:50 -0600

    v2.1
    Falta utilizar la clase paciente y mostrar los datos correspondientes a
    cada prioridad

--- src v2/main.java
+++ src v2/main.java
@@ -16,10 +16,13 @@ public class main {
 	
     public static void main(String[] args) throws Exception{
     	
+    	//Se crea un objeto de la clase VectorHeap
     	VectorHeap vec = new VectorHeap();
     	
         try{
         	BufferedReader in = new BufferedReader(new FileReader("C:/Users/MS/Documents/Java 2/Hoja8/src/pacientes.txt"));
+        	
+        	//Se separan los datos de los pacientes, nombre, sintoma y prioridad
         	String d;
         	d = in.readLine();
         	String[] ar = d.split(",");
@@ -27,6 +30,8 @@ public class main {
         	System.out.println(ar[0]);
         	System.out.println(ar[1]);
         	System.out.println(ar[2]);
+        	
+        	//Se manda la prioridad a VectorHeap
         	vec.add(ar[2]);
         	
         	while((d = in.readLine()) != null){
@@ -35,18 +40,21 @@ public class main {
             	System.out.println(arr[0]);
             	System.out.println(arr[1]);
             	System.out.println(arr[2]);
-            	
+            	//Se manda la prioridad a VectorHeap
             	vec.add(arr[2]);
         	}
         	
         	System.out.println("---");
         	
+        	//Se muestran las prioridades ya ordenadas
         	for (int i = 0; i < 4; i++){
         		System.out.println(vec.poll());
         	}
         	
         	
         } catch (IOException e){
+        	
+        	//Mensaje de error si no se encuentra el archivo
         	System.out.println("Error de lectura");
         }
     }
