0a3733ad5f41d836a28fa82131af7ca7ca0d196d
boggdan95 <bandabog@gmail.com>
2015-10-03 16:33:46 -0600

    Interface
    Se crea la interfaz PriorityQueue

--- src/hojadetrabajo/pkg8/VectorHeap.java
+++ src/hojadetrabajo/pkg8/VectorHeap.java
@@ -1,5 +1,4 @@
-
-import java.util.PriorityQueue;
+package hojadetrabajo.pkg8;
 import java.util.Vector;
 
 /*
@@ -14,13 +13,106 @@ import java.util.Vector;
  *
  * @author Alicia-Lester
  */
-public class VectorHeap <E extends Comparable<E>> extends PriorityQueue<E>{
+public class VectorHeap <E extends Comparable<E>> implements PriorityQueue<E>{
 
-    protected Vector<E> data;
-    
-    public VectorHeap() {
-        
-        data = new Vector<E>();
+	protected Vector<E> data;
+	
+	public VectorHeap(){
+		data = new Vector<E>();
+	}
+	
+	public VectorHeap(Vector<E> v){
+		int i;
+		data = new Vector<E>(v.size());
+		for (i=0; i<v.size(); i++){
+			add(v.get(i));
+		}
+	}
+	
+	protected static int parent(int i){
+		return (i-1)/2;
+	}
+	
+	protected static int left(int i){
+		return 2*i+1;
+	}
+	
+	protected static int right(int i){
+		return 2*(i+1);
+	}
+	
+	protected void percolateUp(int leaf){
+		int parent = parent(leaf);
+		E value = data.get(leaf);
+		while (leaf > 0 && (value.compareTo(data.get(parent)) < 0)){
+			data.set(leaf,data.get(parent));
+			leaf = parent;
+			parent = parent(leaf);
+		}
+		data.set(leaf, value);
+	}
+	
+    /**
+     *
+     * @param value
+     */
+        @Override
+	public void add(E value){
+		data.add(value);
+		percolateUp(data.size()-1);
+	}
+	
+	protected void pushDownRoot(int root){
+		int heapSize = data.size();
+		E value = data.get(root);
+		while (root<heapSize){
+			int childpos = left(root);
+			if(childpos < heapSize){
+				if ((right(root) < heapSize) && ((data.get(childpos+1)).compareTo(data.get(childpos))<0)){
+					childpos++;
+				}
+				if((data.get(childpos)).compareTo(value)<0){
+					data.set(root, data.get(childpos));
+					root = childpos;
+				}
+				else{
+					data.set(root, value);
+					return;
+				}
+			}
+			else{
+				data.set(root, value);
+			}
+		}
+	}
+	
+	public E remove(){
+		E minVal = getFirst();
+		data.set(0, data.get(data.size()-1));
+		data.setSize(data.size()-1);
+		if(data.size() > 1) 
+			pushDownRoot(0);
+		return minVal;
+	}
+
+    @Override
+    public E getFirst() {
+        return data.get(0);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     
